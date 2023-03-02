fe818850b4d0365df77cc3010bfb153c099c6525
zhumeng <zhumeng@cug.edu.cn>
2023-03-02 19:16:37 +0800

    检测系统接入成功

--- app/src/main/java/com/example/cugerhuo/StartSplashActivity.java
+++ app/src/main/java/com/example/cugerhuo/StartSplashActivity.java
@@ -8,6 +8,7 @@ import android.os.Handler;
 import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.baidu.mobstat.StatService;
 import com.example.cugerhuo.Activity.ErHuoActivity;
 
 /**
@@ -25,6 +26,24 @@ public class StartSplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /**
+         * 检测系统启动代码
+         * @author 朱萌
+         * @time 2023/3/2 19：12
+         */
+        StatService.setDebugOn(true);// 开启自动埋点统计
+        // 第三个参数：autoTrackWebview：
+        // 如果设置为true，则自动track所有webview；如果设置为false，则不自动track webview，
+        // 如需对webview进行统计，需要对特定webview调用trackWebView() 即可。
+        // 重要：如果有对webview设置过webchromeclient，则需要调用trackWebView() 接口将WebChromeClient对象传入，
+        // 否则开发者自定义的回调无法收到。
+        StatService.autoTrace(this, true, false);
+
+
+        /**
+         * 启动动画
+         * @author 唐小莉
+         */
         setContentView(R.layout.activity_start_splash);
         //appname=findViewById(R.id.appname);
         lottie=findViewById(R.id.lottie);
