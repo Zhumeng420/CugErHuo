fed25e39260fe86b5aa23407319db0f9f51dc6aa
zhumeng <zhumeng@cug.edu.cn>
2023-03-03 12:46:31 +0800

    注释规范修改

--- app/src/main/java/com/example/cugerhuo/StartSplashActivity.java
+++ app/src/main/java/com/example/cugerhuo/StartSplashActivity.java
@@ -20,7 +20,9 @@ import com.example.cugerhuo.Activity.ErHuoActivity;
  * @author 朱萌
  */
 public class StartSplashActivity extends AppCompatActivity {
-    TextView appname;
+    /**
+     * 动画组件，用于在界面中显示动画
+     */
     LottieAnimationView lottie;
 
     @Override
@@ -32,13 +34,7 @@ public class StartSplashActivity extends AppCompatActivity {
          * @time 2023/3/2 19：12
          */
         StatService.setDebugOn(true);// 开启自动埋点统计
-        // 第三个参数：autoTrackWebview：
-        // 如果设置为true，则自动track所有webview；如果设置为false，则不自动track webview，
-        // 如需对webview进行统计，需要对特定webview调用trackWebView() 即可。
-        // 重要：如果有对webview设置过webchromeclient，则需要调用trackWebView() 接口将WebChromeClient对象传入，
-        // 否则开发者自定义的回调无法收到。
         StatService.autoTrace(this, true, false);
-
 
         /**
          * 启动动画
@@ -46,13 +42,16 @@ public class StartSplashActivity extends AppCompatActivity {
          * @time 2023/3/2 21:47
          */
         setContentView(R.layout.activity_start_splash);
-        //appname=findViewById(R.id.appname);
         lottie=findViewById(R.id.lottie);
 
-        //设置lottie动画的动画效果
-        //translationX() 动画X轴偏移量
-        //setDuration() 设置动画运行时间
-        //setStartDelay() 设置动画延迟时间，此时为0，则不进行延迟
+        /**
+         * 设置lottie动画的动画效果
+        translationX() 动画X轴偏移量
+        setDuration() 设置动画运行时间
+        setStartDelay() 设置动画延迟时间，此时为0，则不进行延迟
+         @author 唐小莉
+         @time 2023/3/2 21:47
+         */
         lottie.animate().translationX(0).setDuration(2000).setStartDelay(0);//
 
 
@@ -66,8 +65,10 @@ public class StartSplashActivity extends AppCompatActivity {
             public void run() {
                 Intent i=new Intent(getApplicationContext(), ErHuoActivity.class);
                 startActivity(i);
-                //跳转到ErHuoActivity界面，并结束当前界面生命周期
-                //当用户在下一个节目点击返回 则直接退出app 而不是返回当前页面
+                /**
+                 * 跳转到ErHuoActivity界面，并结束当前界面生命周期
+                 * 当用户在下一个节目点击返回 则直接退出app 而不是返回当前页面
+                 */
                 finish();
             }
         },9000);
