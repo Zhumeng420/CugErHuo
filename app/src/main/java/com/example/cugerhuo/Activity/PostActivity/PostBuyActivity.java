a312b2d67d18174b7c366797bd55aed5414806e9
zhumeng <zhumeng@cug.edu.cn>
2023-03-18 14:15:09 +0800

    --task=1017107 --user=朱萌 发布模块结构

---
+++ app/src/main/java/com/example/cugerhuo/Activity/PostActivity/PostBuyActivity.java
@@ -0,0 +1,16 @@
+package com.example.cugerhuo.Activity.PostActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cugerhuo.R;
+
+public class PostBuyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_buy);
+    }
+}
