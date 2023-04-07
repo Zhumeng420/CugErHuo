9fc27d7c40be73b4d591a65a50d7f509b7c43e1d
carollkarry <carollkarry@cug.edu.cn>
2023-04-07 22:48:37 +0800

    --task=1017327 --user=carollkarry 关注列表ui界面以及RecyclerViewAdapter适配器

--- app/src/main/java/com/example/cugerhuo/Activity/ConcernActivity.java
+++ app/src/main/java/com/example/cugerhuo/Activity/ConcernActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.cugerhuo.Activity.adapter.Concern;
 import com.example.cugerhuo.Activity.adapter.RecyclerViewAdapter;
 import com.example.cugerhuo.R;
 
@@ -20,14 +21,13 @@ public class ConcernActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_concern);
-        RecyclerView recyclerView1 = findViewById(R.id.recyclerView1);
-        recyclerView1.setLayoutManager(new LinearLayoutManager(this));
-        RecyclerViewAdapter adapter1 = new RecyclerViewAdapter(this,"RecyclerView1-");
-        recyclerView1.setAdapter(adapter1);
 
+        //concern是自定义的存储关注列表的用户信息的类
+        Concern concern=new Concern("RecyclerView2-","xxxx","你好");
         RecyclerView recyclerView2 = findViewById(R.id.recyclerView2);
         recyclerView2.setLayoutManager(new LinearLayoutManager(this));
-        RecyclerViewAdapter adapter2 = new RecyclerViewAdapter(this,"RecyclerView2-");
+        RecyclerViewAdapter adapter2 = new RecyclerViewAdapter(this,concern);
         recyclerView2.setAdapter(adapter2);
+
     }
 }
--- app/src/main/java/com/example/cugerhuo/Activity/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/cugerhuo/Activity/adapter/RecyclerViewAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,23 +14,36 @@ import android.widget.Toast;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cugerhuo.R;
+import com.makeramen.roundedimageview.RoundedImageView;
 
 /**
  * @Author 唐小莉
- * @Description 为自定义RecyclerView适配器
+ * @Description 为自定义RecyclerView适配器,适配关注列表信息
  * @Date 2023/4/4 22：13
  */
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
+    /**
+     * concern 关注的用户
+     * count 关注列表的Item个数，默认为30（为了前端调试）
+     * @author 唐小莉
+     * @Time 2023/4/7 22：16
+     */
     private Context context;
-    private String text;
+    private Concern concern;
     private int count = 30;
 
-    public RecyclerViewAdapter(Context context, String text) {
+    public RecyclerViewAdapter(Context context, Concern concern) {
         this.context = context;
-        this.text = text;
+        this.concern=concern;
     }
 
+    /**
+     * 获取目前RecyclerView中的item数量
+     * @return 数量
+     * @author 唐小莉
+     * @time 2023/4/7 22:26
+     */
     @Override
     public int getItemCount() {
         return count;
@@ -37,12 +52,24 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     public int getCount() {
         return count;
     }
-
+    /**
+     * 设置目前RecyclerView中的item数量
+     * @author 唐小莉
+     * @time 2023/4/7 22:26
+     */
     public void setCount(int count) {
         this.count = count;
         notifyDataSetChanged();
     }
 
+    /**
+     * 对应每个子view item
+     * @param parent
+     * @param viewType
+     * @return
+     * @author 唐小莉
+     * @Time 2023/4/7 17：23
+     */
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.item_concern, parent, false);
@@ -51,23 +78,46 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
-        holder.textView.setText(text + (position + 1));
-        holder.textView.setOnClickListener(new View.OnClickListener() {
+        //进行item对应控件部分的内容设置
+        holder.user_concern_name.setText(concern.getName() + (position + 1));
+        holder.user_concern_sign.setText(concern.getConcern_desc()+(position+1));
+        holder.user_concern_name.setOnClickListener(new View.OnClickListener() {
+            /**
+             * 点击每个RecyclerView子组件进行相应的响应事件
+             * @param v
+             * @author
+             * @Time
+             */
             @Override
             public void onClick(View v) {
-                Toast.makeText(context,holder.textView.getText().toString(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(context,holder.user_concern_name.getText().toString(),Toast.LENGTH_SHORT).show();
+
             }
         });
+
     }
 
+    /**
+     * 寻找到对应控件的id
+     * user_concern_name 用户昵称
+     * user_concern_img 用户头像
+     * user_concern_sign 用户介绍
+     * btn_concerned 已关注按钮
+     * @author 唐小莉
+     * @Time 2023/4/7 17:25
+     */
     public static class ViewHolder extends RecyclerView.ViewHolder {
-
-        TextView textView;
+        TextView user_concern_name;
+        RoundedImageView user_concern_img;
+        TextView user_concern_sign;
+        Button btn_concerned;
 
         public ViewHolder(View itemView) {
             super(itemView);
-
-            textView = itemView.findViewById(R.id.textView);
+            user_concern_name = itemView.findViewById(R.id.user_concern_name);
+            user_concern_img=itemView.findViewById(R.id.user_concern_img);
+            user_concern_sign=itemView.findViewById(R.id.user_concern_sign);
+            btn_concerned=itemView.findViewById(R.id.btn_concerned);
         }
     }
 
