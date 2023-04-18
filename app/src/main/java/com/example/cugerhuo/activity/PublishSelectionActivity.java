9c7202ded61d4e17120b3b281eb2bcb301748aad
lbramerix <363127772@qq.com>
2023-04-18 19:50:51 +0800

    --task=1018236 --user=李柏睿 发布页

--- app/src/main/java/com/example/cugerhuo/activity/PublishSelectionActivity.java
+++ app/src/main/java/com/example/cugerhuo/activity/PublishSelectionActivity.java
@@ -2,6 +2,7 @@ package com.example.cugerhuo.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
@@ -12,16 +13,21 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.example.cugerhuo.R;
+import com.example.cugerhuo.activity.imessage.MessageActivity;
+import com.example.cugerhuo.activity.post.PostBuyActivity;
+import com.example.cugerhuo.activity.post.PostSellActivity;
+
 /**
  * 发布选择
  * @Author: 李柏睿
  * @Time: 2023/3/22 15:40
  */
-public class PublishSelectionActivity extends AppCompatActivity {
+public class PublishSelectionActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ImageView iv_tab_three;
     private AlphaAnimation alphaAniShow, alphaAniHide;
     private LinearLayout AlphaShow;
+    private LinearLayout postBuy,postSell;
     private TranslateAnimation translateAniShow, translateAniHide;
 
     @Override
@@ -29,16 +35,18 @@ public class PublishSelectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publish_selection);
         initView();
-
         iv_tab_three.setOnClickListener(this::onClickExit);
     }
 
     public void initView(){
+        postBuy = findViewById(R.id.publish_select_buy);
+        postBuy.setOnClickListener(this);
+        postSell = findViewById(R.id.publish_select_sell);
+        postSell.setOnClickListener(this);
         iv_tab_three = (ImageView) findViewById(R.id.iv_tab_three);
         AlphaShow = (LinearLayout) findViewById(R.id.publish_select);
 //        aa.setBackgroundDrawable(getResources().getDrawable(R.drawable.icon_2))
         iv_tab_three.setRotation(45);
-
         translateAnimation();
         AlphaShow.startAnimation(translateAniShow);
         AlphaShow.setVisibility(View.VISIBLE);
@@ -54,6 +62,38 @@ public class PublishSelectionActivity extends AppCompatActivity {
 //        animation.setDuration( 50 );
 //        aa.startAnimation( animation );
     }
+
+    /**
+     * 买卖点击事件跳转
+     * @Author: 李柏睿
+     * @Time: 2023/4/13 17:12
+     */
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            /**
+             * 点击卖东西事件跳转
+             * @Author: 李柏睿
+             * @Time: 2023/4/13 17:13
+             */
+            case R.id.publish_select_buy:
+                startActivity(new Intent(getApplicationContext(), PostBuyActivity.class));
+                overridePendingTransition(0,0);
+                break;
+            /**
+             * 点击买东西事件跳转
+             * @Author: 李柏睿
+             * @Time: 2023/4/13 17:14
+             */
+            case R.id.publish_select_sell:
+                startActivity(new Intent(getApplicationContext(), PostSellActivity.class));
+                overridePendingTransition(0,0);
+                break;
+            default:
+                break;
+        }
+    }
+
     /**
      * 透明度过度动画
      * @Author: 李柏睿
@@ -61,8 +101,9 @@ public class PublishSelectionActivity extends AppCompatActivity {
      */
     private void alphaAnimation() {
         //显示
-        alphaAniShow = new AlphaAnimation(0, 1);//百分比透明度，从0%到100%显示
-        alphaAniShow.setDuration(800);//一秒
+        //百分比透明度，从0%到100%显示
+        alphaAniShow = new AlphaAnimation(0, 1);
+        alphaAniShow.setDuration(800);
         //隐藏
         alphaAniHide = new AlphaAnimation(1, 0);
         alphaAniHide.setDuration(1000);
@@ -77,27 +118,29 @@ public class PublishSelectionActivity extends AppCompatActivity {
 
         //向上位移显示动画  从自身位置的最下端向上滑动了自身的高度
         translateAniShow = new TranslateAnimation(
-                Animation.RELATIVE_TO_SELF,//RELATIVE_TO_SELF表示操作自身
-                0,//fromXValue表示开始的X轴位置
+                //RELATIVE_TO_SELF表示操作自身,fromXValue表示开始的X轴位置,fromXValue表示结束的X轴位置,fromXValue表示开始的Y轴位置,fromXValue表示结束的Y轴位置
                 Animation.RELATIVE_TO_SELF,
-                0,//fromXValue表示结束的X轴位置
+                0,
                 Animation.RELATIVE_TO_SELF,
-                1,//fromXValue表示开始的Y轴位置
+                0,
                 Animation.RELATIVE_TO_SELF,
-                0);//fromXValue表示结束的Y轴位置
+                1,
+                Animation.RELATIVE_TO_SELF,
+                0);
         translateAniShow.setRepeatMode(Animation.REVERSE);
         translateAniShow.setDuration(200);
 
         //向下位移隐藏动画  从自身位置的最上端向下滑动了自身的高度
         translateAniHide = new TranslateAnimation(
-                Animation.RELATIVE_TO_SELF,//RELATIVE_TO_SELF表示操作自身
-                0,//fromXValue表示开始的X轴位置
+                //RELATIVE_TO_SELF表示操作自身,fromXValue表示开始的X轴位置,fromXValue表示结束的X轴位置,fromXValue表示开始的Y轴位置,fromXValue表示结束的Y轴位置
                 Animation.RELATIVE_TO_SELF,
-                0,//fromXValue表示结束的X轴位置
+                0,
                 Animation.RELATIVE_TO_SELF,
-                0,//fromXValue表示开始的Y轴位置
+                0,
                 Animation.RELATIVE_TO_SELF,
-                1);//fromXValue表示结束的Y轴位置
+                0,
+                Animation.RELATIVE_TO_SELF,
+                1);
         translateAniHide.setRepeatMode(Animation.REVERSE);
         translateAniHide.setDuration(200);
     }
@@ -127,8 +170,8 @@ public class PublishSelectionActivity extends AppCompatActivity {
             @Override
             public void onAnimationRepeat(Animation animation) {}
         });
-
-
     }
+
+
 
 }
