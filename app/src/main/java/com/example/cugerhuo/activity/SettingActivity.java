141154d1e2831f84997703538d893638764bd99c
abcd <1205126916@qq.com>
2023-04-19 14:59:48 +0800

    --task=1018730 --user=施立豪 删除缓存bug修复

--- app/src/main/java/com/example/cugerhuo/activity/SettingActivity.java
+++ app/src/main/java/com/example/cugerhuo/activity/SettingActivity.java
@@ -1,7 +1,5 @@
 package com.example.cugerhuo.activity;
 
-import static com.netease.nim.highavailable.HighAvailableObject.getContext;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -9,6 +7,7 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cugerhuo.R;
+import com.example.cugerhuo.access.user.UserInfo;
 import com.example.cugerhuo.tools.MyToast;
 
 import java.io.File;
@@ -50,13 +49,17 @@ public class SettingActivity extends AppCompatActivity {
     {   //打开文件目录
         File file=new File(getSandboxPath());
         //当前文件是目录，删除目录下所有文件
+        System.out.println((getSandboxPath()));
+        System.out.println("path"+(getSandboxPath()));
+        System.out.println("image"+ UserInfo.getUrl());
         if (file.isDirectory()) {
             File[] files = file.listFiles();
             for (int i = 0; i < files.length; i++) {
                 File f = files[i];
+                System.out.println(i);
                 //如果文件存在则删除
-                if (file.exists()) {
-                    file.delete();
+                if (f.exists()) {
+                    f.delete();
             }
             file.delete();//如要保留文件夹，只删除文件，请注释这行
         }MyToast.toast(this,"清除缓存成功",3);
@@ -68,7 +71,7 @@ public class SettingActivity extends AppCompatActivity {
      * @return 路径字符串
      */
     private String getSandboxPath() {
-        File externalFilesDir = getContext().getExternalFilesDir("");
+        File externalFilesDir = SettingActivity.this.getExternalFilesDir("");
         File customFile = new File(externalFilesDir.getAbsolutePath(), "Sandbox");
         if (!customFile.exists()) {
             customFile.mkdirs();
