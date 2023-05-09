eedc0377e38e7be10da1a58c0bf4a6a2cdc931c5
carollkarry <carollkarry@cug.edu.cn>
2023-05-09 14:45:15 +0800

    --task=1017327 --user=carollkarry 我的评价ui

---
+++ app/src/main/java/com/example/cugerhuo/activity/mycenter/EvaluateActivity.java
@@ -0,0 +1,19 @@
+package com.example.cugerhuo.activity.mycenter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cugerhuo.R;
+
+/**
+ * @author carollkarry
+ */
+public class EvaluateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evaluate);
+    }
+}
